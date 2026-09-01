ee8fb7fc164142eb0c76650759d01d1791fed241
Vishal <vishalkannaujiya230310558313@gmail.com>
2026-09-01 18:11:55 +0530

    fix: add pg to serverExternalPackages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['better-sqlite3'],
+  serverExternalPackages: ['better-sqlite3', 'pg'],
 };
 
 export default nextConfig;
